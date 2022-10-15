c30bcded3c5be46ce3e077696e51ad69f3747451
santost12 <45154554+santost12@users.noreply.github.com>
2022-10-15 13:15:04 -0400

    add firefox-test.py

---
+++ firefox-test.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service
+from webdriver_manager.firefox import GeckoDriverManager
+
+driver = webdriver.Firefox(service=Service(executable_path=GeckoDriverManager().install()))
+
+driver.get("https://www.google.com/")
+
+driver.quit()
